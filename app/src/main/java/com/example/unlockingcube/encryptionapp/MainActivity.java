2a0142a6258a47cb0e76384595f352ff16a33246
Mark Tong <tongm@email.sc.edu>
2015-11-21 19:10:03 -0500

    Added a class to encrypt the plaintext using AES.

--- app/src/main/java/com/example/unlockingcube/encryptionapp/MainActivity.java
+++ app/src/main/java/com/example/unlockingcube/encryptionapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.unlockingcube.encryptionapp;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +13,13 @@ import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
 
+
+import java.security.SecureRandom;
 import java.util.ArrayList;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends Activity {
  String type = "Caesors";
@@ -22,12 +29,12 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         loadSpinners(this);
 
-        Button decryptButton = (Button)findViewById(R.id.ebutton);
+        Button encryptButton = (Button)findViewById(R.id.ebutton);
 
-        decryptButton.setOnClickListener(
+        encryptButton.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                    EncrptCaesors();
+                    AESEncrypt();
                     }
                 }
         );
@@ -71,7 +78,7 @@ public class MainActivity extends Activity {
         switch(type)
         {
             case "Caesors":
-                EncrptCaesors();//calls Caesors encrypt algorithom. - this is not implemented right now
+                AESEncrypt();//calls Caesors encrypt algorithom. - this is not implemented right now
                 break;
             case "AES": //calls AES encrypt
                 break;
@@ -84,93 +91,138 @@ public class MainActivity extends Activity {
         }
 
     }
-    public void EncrptCaesors(){
+//    public void EncrptCaesors(){
+//
+//        TextView ptextview = (TextView)findViewById(R.id.PlainText);
+//        String plaintext ="";
+//        plaintext = ptextview.getText().toString();
+//        String encryptedText ="";
+//        //This needs to be checked for null pointers
+//        TextView PwordText = (TextView)findViewById(R.id.password);
+//        int  k = Integer.parseInt(PwordText.getText().toString());
+//        ArrayList<Character> List = new ArrayList<Character>(52);
+//        ArrayList<Character> Etext = new ArrayList<Character>(plaintext.length());
+//        List.add('A');
+//        List.add('a');
+//        List.add('B');
+//        List.add('b');
+//        List.add('C');
+//        List.add('c');
+//        List.add('D');
+//        List.add('d');
+//        List.add('E');
+//        List.add('e');
+//        List.add('F');
+//        List.add('f');
+//        List.add('G');
+//        List.add('g');
+//        List.add('H');
+//        List.add('h');
+//        List.add('I');
+//        List.add('i');
+//        List.add('J');
+//        List.add('j');
+//        List.add('K');
+//        List.add('k');
+//        List.add('L');
+//        List.add('l');
+//        List.add('M');
+//        List.add('m');
+//        List.add('N');
+//        List.add('n');
+//        List.add('O');
+//        List.add('o');
+//        List.add('P');
+//        List.add('p');
+//        List.add('Q');
+//        List.add('q');
+//        List.add('R');
+//        List.add('r');
+//        List.add('S');
+//        List.add('s');
+//        List.add('T');
+//        List.add('t');
+//        List.add('W');
+//        List.add('w');
+//        List.add('X');
+//        List.add('x');
+//        List.add('Y');
+//        List.add('y');
+//        List.add('Z');
+//        List.add('z');
+//
+//        for(int i = 0; i< plaintext.length(); i++ )
+//        {
+//            plaintext.charAt(i);
+//            for(int j = 0; j < List.size(); j++)
+//            {
+//                int shift = k + j;
+//                while(shift > 52)
+//                {
+//                    shift -= 52;
+//                }
+//                if(List.equals(plaintext.charAt(i)))
+//                {
+//                  Etext.add(List.get(shift));
+//
+//                }
+//            }
+//
+//        }
+//        for(int i =0; i < Etext.size(); i++)
+//        {
+//          encryptedText += Etext.get(i);
+//        }
+//        TextView Etextview = (TextView)findViewById(R.id.EncryptedText);
+//        //fails somewhere in the shifting process and outputs a blank string
+////        Etextview.setText(encryptedText);
+//
+//        Etextview.setText("SOMETHING");
+//
+//    }
 
-        TextView ptextview = (TextView)findViewById(R.id.PlainText);
-        String plaintext ="";
-        plaintext = ptextview.getText().toString();
-        String encryptedText ="";
-        //This needs to be checked for null pointers
-        TextView PwordText = (TextView)findViewById(R.id.password);
-        int  k = Integer.parseInt(PwordText.getText().toString());
-        ArrayList<Character> List = new ArrayList<Character>(52);
-        ArrayList<Character> Etext = new ArrayList<Character>(plaintext.length());
-        List.add('A');
-        List.add('a');
-        List.add('B');
-        List.add('b');
-        List.add('C');
-        List.add('c');
-        List.add('D');
-        List.add('d');
-        List.add('E');
-        List.add('e');
-        List.add('F');
-        List.add('f');
-        List.add('G');
-        List.add('g');
-        List.add('H');
-        List.add('h');
-        List.add('I');
-        List.add('i');
-        List.add('J');
-        List.add('j');
-        List.add('K');
-        List.add('k');
-        List.add('L');
-        List.add('l');
-        List.add('M');
-        List.add('m');
-        List.add('N');
-        List.add('n');
-        List.add('O');
-        List.add('o');
-        List.add('P');
-        List.add('p');
-        List.add('Q');
-        List.add('q');
-        List.add('R');
-        List.add('r');
-        List.add('S');
-        List.add('s');
-        List.add('T');
-        List.add('t');
-        List.add('W');
-        List.add('w');
-        List.add('X');
-        List.add('x');
-        List.add('Y');
-        List.add('y');
-        List.add('Z');
-        List.add('z');
+    public void AESEncrypt() {
+    // Original text
+    //String theTestText = "This is just a simple test";
+    TextView plainTextView = (TextView)findViewById(R.id.PlainText);
+        String plainText = plainTextView.getText().toString();
 
-        for(int i = 0; i< plaintext.length(); i++ )
-        {
-            plaintext.charAt(i);
-            for(int j = 0; j < List.size(); j++)
-            {
-                int shift = k + j;
-                while(shift > 52)
-                {
-                    shift -= 52;
-                }
-                if(List.equals(plaintext.charAt(i)))
-                {
-                  Etext.add(List.get(shift));
-
-                }
-            }
-
-        }
-        for(int i =0; i < Etext.size(); i++)
-        {
-          encryptedText += Etext.get(i);
-        }
-        TextView Etextview = (TextView)findViewById(R.id.EncryptedText);
-        //fails somewhere in the shifting process and outputs a blank string
-//        Etextview.setText(encryptedText);
-
-        Etextview.setText("SOMETHING");
-
+    // Set up secret key spec for 128-bit AES encryption and decryption
+    SecretKeySpec sks = null;
+    try {
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+        sr.setSeed("any data used as random seed".getBytes());
+        KeyGenerator kg = KeyGenerator.getInstance("AES");
+        kg.init(128, sr);
+        sks = new SecretKeySpec((kg.generateKey()).getEncoded(), "AES");
+    } catch (Exception e) {
+//        Log.e(TAG, "AES secret key spec error");
     }
+
+    // Encode the original data with AES
+    byte[] encodedBytes = null;
+    try {
+        Cipher c = Cipher.getInstance("AES");
+        c.init(Cipher.ENCRYPT_MODE, sks);
+        encodedBytes = c.doFinal(plainText.getBytes());
+    } catch (Exception e) {
+//        Log.e(TAG, "AES encryption error");
+    }
+   TextView encryptedText = (TextView)findViewById(R.id.EncryptedText);
+    encryptedText.setText("[ENCODED]:\n" +
+            Base64.encodeToString(encodedBytes, Base64.DEFAULT) + "\n");
+
+    // Decode the encoded data with AES
+    byte[] decodedBytes = null;
+    try {
+        Cipher c = Cipher.getInstance("AES");
+        c.init(Cipher.DECRYPT_MODE, sks);
+        decodedBytes = c.doFinal(encodedBytes);
+    } catch (Exception e) {
+//        Log.e(TAG, "AES decryption error");
+    }
+//    TextView tvdecoded = (TextView)findViewById(R.id.tvdecoded);
+        TextView decryptedText = (TextView)findViewById(R.id.DecryptedText);
+        decryptedText.setText("[DECODED]:\n" + new String(decodedBytes) + "\n");
+}
 }
